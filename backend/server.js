188a5e64d60e1999db297961a6665dcf9eaba31f
aryan06-blip <aryan.2428cseaiml273@kiet.edu>
2026-05-18 11:22:49 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const path = require('path');
 // Serve frontend in production
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
 });
 
